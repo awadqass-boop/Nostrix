7224d7da5c83597b081868169337793d3e0132c6
awadqass-boop <awadqass@gmail.com>
2026-08-06 08:32:38 +0400

    Add Google Analytics ID

--- site-config.js
+++ site-config.js
@@ -1,2 +1,4 @@
 window.NOSTRIX_REVIEWS_WEB_APP_URL =
   'https://script.google.com/macros/s/AKfycbw75fLJn2T-ekJsoGh359dCKGae-yLT2nfsJwcMCtJ_k4i97ckFnOz_7xZ7k0Q81qkKjw/exec';
+window.NOSTRIX_GA_MEASUREMENT_ID =
+  'G-DJ4Y5NSNH5';
