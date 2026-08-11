8064a2104b1375c88763a7b819eacb449a4a8c13
awadqass-boop <awadqass@gmail.com>
2026-08-11 20:02:33 +0400

    Connect staff portal to Supabase

---
+++ staff/config.js
@@ -0,0 +1,5 @@
+window.NOSTRIX_STAFF_CONFIG = {
+  SUPABASE_URL: "https://kqigwdyhaedlaohdegov.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_CFG-ajAZt7eb_HFItIY1NQ_APs51pQO",
+  STAFF_PATH: "/staff/"
+};
